fb59e39192ec70cdaf433b708ed935dc23ce8f33
Greyson Meares <greysonmeares@gmail.com>
2025-05-02 14:37:38 -0400

    fix to rank

--- rank.py
+++ rank.py
@@ -58,9 +58,21 @@ def rank(zip_code, search_radius, comparator_weights, launch):
     if user_coords is None:
         return "invalid zip code", 400
 
+        user_coords = zip_to_coords(zip_code)
+    if user_coords is None:
+        return "invalid zip code", 400
+
     #get sites in the search radius
     valid_sites = sites_in_radius(user_coords, search_radius)
+    # print (f"Valid sites: {valid_sites}")
+
+    # print (f"User coords: {user_coords}")
+    # print (f"Comparator weights: {comparator_weights}")
+    # print (f"Launch: {launch}")
+
     site_objects = [SiteData(site) for site in valid_sites]
+    for site in site_objects:
+        print (f"Site object: {site}")
 
     # Get forecast data for each valid site
     forcast_data = {}
@@ -70,18 +82,30 @@ def rank(zip_code, search_radius, comparator_weights, launch):
         forcast_data[site] = forcast
         if forcast is None:
             return "invalid forecast data", 400
+        
+    # for site, forecast in forcast_data.items():
+    #     print (f"Forecast data for SITE: {site.zip_code}: {forecast}")
 
     weight_vector = np.array([comparator_weights[comp] for comp in valid_comparators])
     print (f"Weight vector: {weight_vector}")
 
-    #normalize the weights
+    #normalize the weights      (L^1 norm here because we want to sum to 1)
     weight_vector = weight_vector / np.linalg.norm(weight_vector, ord=1)
 
     print (f"Normalized weight vector: {weight_vector}")
 
+    # for comp in valid_comparators:
+    #     ranking = Comparators.compare_sites(launch, forcast_data, comp) 
+    #     print (f"Ranking for {comp}: {ranking}")
+
+    # Make comparison input dictionary  SiteData -> ForecastData
+    sites_with_forecasts = {}
+
     # for each comparator, rank the launch sites
     # using the given weights, find the average rank for each site
     rankings = {}   # Comparator -> List of SiteData
+
+
     
     print ("Running comparators...")
     for key, weight in comparator_weights.items():
@@ -103,7 +127,10 @@ def rank(zip_code, search_radius, comparator_weights, launch):
     print ("\n\n\n")
     print ("AVERAGing ranks...")
     for key, weight in comparator_weights.items():
-        avg_rankings = weight_vector * rankings[key]
+        for site in rankings[key]:
+            if site not in avg_rankings:
+                avg_rankings[site] = 0
+            avg_rankings[site] += weight * rankings[key].index(site)
 
     #print average rankings
     print ("-------------")
@@ -119,5 +146,4 @@ def rank(zip_code, search_radius, comparator_weights, launch):
         print (f"Site: {site.zip_code}, Average rank: {avg_rankings[site]}")
 
     return sorted_sites, 200
-
       
