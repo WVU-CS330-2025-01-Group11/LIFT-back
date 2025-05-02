c8260cd92f1eddc1102d11f23aef17f633b299fb
Greyson Meares <greysonmeares@gmail.com>
2025-05-02 12:31:31 -0400

    minor clean up

--- rank.py
+++ rank.py
@@ -60,15 +60,7 @@ def rank(zip_code, search_radius, comparator_weights, launch):
 
     #get sites in the search radius
     valid_sites = sites_in_radius(user_coords, search_radius)
-    # print (f"Valid sites: {valid_sites}")
-
-    # print (f"User coords: {user_coords}")
-    # print (f"Comparator weights: {comparator_weights}")
-    # print (f"Launch: {launch}")
-
     site_objects = [SiteData(site) for site in valid_sites]
-    for site in site_objects:
-        print (f"Site object: {site}")
 
     # Get forecast data for each valid site
     forcast_data = {}
@@ -78,24 +70,14 @@ def rank(zip_code, search_radius, comparator_weights, launch):
         forcast_data[site] = forcast
         if forcast is None:
             return "invalid forecast data", 400
-        
-    # for site, forecast in forcast_data.items():
-    #     print (f"Forecast data for SITE: {site.zip_code}: {forecast}")
 
     weight_vector = np.array([comparator_weights[comp] for comp in valid_comparators])
     print (f"Weight vector: {weight_vector}")
 
-    #normalize the weights      (L^1 norm here because we want to sum to 1)
+    #normalize the weights
     weight_vector = weight_vector / np.linalg.norm(weight_vector, ord=1)
 
     print (f"Normalized weight vector: {weight_vector}")
-
-    # for comp in valid_comparators:
-    #     ranking = Comparators.compare_sites(launch, forcast_data, comp) 
-    #     print (f"Ranking for {comp}: {ranking}")
-
-    # Make comparison input dictionary  SiteData -> ForecastData
-    sites_with_forecasts = {}
 
     # for each comparator, rank the launch sites
     # using the given weights, find the average rank for each site
@@ -122,10 +104,6 @@ def rank(zip_code, search_radius, comparator_weights, launch):
     print ("AVERAGing ranks...")
     for key, weight in comparator_weights.items():
         avg_rankings = weight_vector * rankings[key]
-        # for site in rankings[key]:
-        #     if site not in avg_rankings:
-        #         avg_rankings[site] = 0
-        #     avg_rankings[site] += weight * rankings[key].index(site)
 
     #print average rankings
     print ("-------------")
